205c9165ef6739272249a10a7438420bcb846618
DarkSideMoon <16blackwood16@gmail.com>
2018-02-28 23:48:07 +0200

    Add project files

---
+++ Polyalphabetic.Library/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Polyalphabetic.Library
+{
+    public class Class1
+    {
+    }
+}
---
+++ Polyalphabetic.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Polyalphabetic.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(5 == 5);
+        }
+    }
+}
