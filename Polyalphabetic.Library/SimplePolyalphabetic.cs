0eeb8fef7207c454908f546d49f4854cf79dee89
DarkSideMoon <acubens96@meta.ua>
2018-03-03 16:55:20 +0200

    Add tests to simple polyalphabetic

--- Polyalphabetic.Library/SimplePolyalphabetic.cs
+++ Polyalphabetic.Library/SimplePolyalphabetic.cs
@@ -26,7 +26,7 @@ namespace Polyalphabetic.Library
         /// </summary>
         private readonly char[] _characters = new char[] 
         {
-            ' ',
+            ' ', '?', '!',
             '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
             'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
--- Polyalphabetic.Tests/SimplePolyalphabeticTest.cs
+++ Polyalphabetic.Tests/SimplePolyalphabeticTest.cs
@@ -9,12 +9,93 @@ namespace Polyalphabetic.Tests
     [TestClass]
     public class SimplePolyalphabeticTest
     {
+        private SimplePolyalphabetic polyalphabetic;
+
+        public SimplePolyalphabeticTest()
+        {
+            polyalphabetic = new SimplePolyalphabetic();
+        }
+
+
         [TestMethod]
         public void TestCountOfAlphabet()
         {
-            SimplePolyalphabetic polyalphabetic = new SimplePolyalphabetic();
+            Assert.IsTrue(polyalphabetic.AlphabetCount == 38);
+        }
 
-            Assert.IsTrue(polyalphabetic.AlphabetCount == 36);
+        [TestMethod]
+        public void TestCodingDecoding1()
+        {
+            // arrange
+            string key = "KEY";
+            string word = "HELLO WORLD";
+
+            // act 
+            string encodeString = polyalphabetic.Encode(word, key);
+            string decodeString = polyalphabetic.Decode(encodeString, key);
+
+            // assert
+            Assert.AreEqual(decodeString, word);
+        }
+
+        [TestMethod]
+        public void TestCodingDecoding2()
+        {
+            // arrange
+            string key = "hi";
+            string word = "hello";
+
+            // act 
+            string encodeString = polyalphabetic.Encode(word, key);
+            string decodeString = polyalphabetic.Decode(encodeString, key);
+
+            // assert
+            Assert.AreEqual(decodeString.ToLower(), word);
+        }
+
+        [TestMethod]
+        public void TestCodingDecoding3()
+        {
+            // arrange
+            string key = "SIMPLE KEY";
+            string word = "simple text 123";
+
+            // act 
+            string encodeString = polyalphabetic.Encode(word, key);
+            string decodeString = polyalphabetic.Decode(encodeString, key);
+
+            // assert
+            Assert.AreEqual(decodeString.ToLower(), word);
+        }
+
+        [TestMethod]
+        public void TestCodingDecoding4()
+        {
+            // arrange
+            string key = "SIMPLE KEY";
+            string word = "123";
+
+            // act 
+            string encodeString = polyalphabetic.Encode(word, key);
+            string decodeString = polyalphabetic.Decode(encodeString, key);
+
+            // assert
+            Assert.AreEqual(decodeString.ToLower(), word);
+        }
+
+        [TestMethod]
+        public void TestCodingDecoding5()
+        {
+            // arrange
+            string key = "key";
+            string word = "how are you?";
+
+            // act 
+            string encodeString = polyalphabetic.Encode(word, key);
+            string decodeString = polyalphabetic.Decode(encodeString, key);
+
+            // assert
+            Assert.AreEqual(decodeString.ToLower(), word);
         }
     }
 }
